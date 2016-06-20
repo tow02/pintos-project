f12eb0f69896cb75279529033fcfba50e8f922f2
Paween Theanvanichpant <paween.the@gmail.com>
2016-06-20 20:22:53 +0700

    Add halt()

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,18 +3,25 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void
+halt (void)
+{
+  shutdown_power_off();
 }
